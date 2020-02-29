2f74736ecdc0c5d13e37e8d695afdc4ed75e6f47
revathi44 <revathichanti123@gmail.com>
2020-02-29 12:50:31 +0530

    adding githubproject

---
+++ GithubProject/src/com/testprogram/Mutliplication.java
@@ -0,0 +1,17 @@
+package com.testprogram;
+
+public class Mutliplication {
+int a=10,b=20,c;
+public void mul()
+{
+	c=a*b;
+	System.out.println(c);
+}
+public static void main(String[] args) {
+	Mutliplication mp=new Mutliplication();
+	mp.mul();
+		// TODO Auto-generated method stub
+
+	}
+
+}
